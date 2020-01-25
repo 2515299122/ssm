a3c14c0c0815c98fe978d190960b901a55ed2b6a
wubing <2515299122@qq.com>
2020-01-25 17:43:41 +0800

    添加所需数据库的sql文件

---
+++ ssm_pro.sql
@@ -0,0 +1,178 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.22-log : Database - ssm_pro
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ssm_pro` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ssm_pro`;
+
+/*Table structure for table `member` */
+
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `nickName` varchar(32) DEFAULT NULL,
+  `phoneNum` varchar(32) DEFAULT NULL,
+  `email` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `order_traveller` */
+
+DROP TABLE IF EXISTS `order_traveller`;
+
+CREATE TABLE `order_traveller` (
+  `orderId` int(32) DEFAULT NULL,
+  `travellerId` int(32) DEFAULT NULL,
+  KEY `orderId` (`orderId`),
+  KEY `travellerId` (`travellerId`),
+  CONSTRAINT `order_traveller_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_traveller_ibfk_2` FOREIGN KEY (`travellerId`) REFERENCES `traveller` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `orders` */
+
+DROP TABLE IF EXISTS `orders`;
+
+CREATE TABLE `orders` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `orderNum` varchar(32) DEFAULT NULL,
+  `orderTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `peopleCount` int(6) DEFAULT NULL,
+  `orderDesc` varchar(500) DEFAULT NULL,
+  `payType` int(1) DEFAULT NULL,
+  `orderStatus` int(1) DEFAULT NULL,
+  `productId` int(32) DEFAULT NULL,
+  `memberId` int(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `memberId` (`memberId`),
+  KEY `productId` (`productId`),
+  CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`memberId`) REFERENCES `member` (`id`),
+  CONSTRAINT `orders_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `permission` */
+
+DROP TABLE IF EXISTS `permission`;
+
+CREATE TABLE `permission` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `permissionName` varchar(10) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `product` */
+
+DROP TABLE IF EXISTS `product`;
+
+CREATE TABLE `product` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `productNum` varchar(50) NOT NULL,
+  `productName` varchar(50) DEFAULT NULL,
+  `cityName` varchar(50) DEFAULT NULL,
+  `departureTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `productPrice` double(10,0) DEFAULT NULL,
+  `productDesc` varchar(500) DEFAULT NULL,
+  `productStatus` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(12) DEFAULT NULL,
+  `roleDesc` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `role_permission` */
+
+DROP TABLE IF EXISTS `role_permission`;
+
+CREATE TABLE `role_permission` (
+  `permissionId` int(32) DEFAULT NULL,
+  `roleId` int(32) DEFAULT NULL,
+  KEY `roleId` (`roleId`),
+  KEY `permissionId` (`permissionId`),
+  CONSTRAINT `role_permission_ibfk_1` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`),
+  CONSTRAINT `role_permission_ibfk_2` FOREIGN KEY (`permissionId`) REFERENCES `permission` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `syslog` */
+
+DROP TABLE IF EXISTS `syslog`;
+
+CREATE TABLE `syslog` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `visitTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `username` varchar(16) DEFAULT NULL,
+  `ip` varchar(16) DEFAULT NULL,
+  `url` varchar(32) DEFAULT NULL,
+  `executionTime` int(16) DEFAULT NULL,
+  `method` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=516 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `traveller` */
+
+DROP TABLE IF EXISTS `traveller`;
+
+CREATE TABLE `traveller` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL,
+  `sex` varchar(32) DEFAULT NULL,
+  `phoneNum` varchar(32) DEFAULT NULL,
+  `credentialstype` int(1) DEFAULT NULL,
+  `credentialsnum` varchar(32) DEFAULT NULL,
+  `travellerType` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(32) NOT NULL AUTO_INCREMENT,
+  `email` varchar(30) DEFAULT NULL,
+  `username` varchar(30) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `phoneNum` varchar(16) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `icon` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Table structure for table `users_role` */
+
+DROP TABLE IF EXISTS `users_role`;
+
+CREATE TABLE `users_role` (
+  `userId` int(32) DEFAULT NULL,
+  `roleId` int(32) DEFAULT NULL,
+  KEY `userId` (`userId`),
+  KEY `roleId` (`roleId`),
+  CONSTRAINT `users_role_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`),
+  CONSTRAINT `users_role_ibfk_2` FOREIGN KEY (`roleId`) REFERENCES `role` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
